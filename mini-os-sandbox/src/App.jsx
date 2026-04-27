6bd357766dbc198d9b6498a166e17a2c2d3e0199
Michael Rey Belga <mrab2023-5629-60654@bicol-u.edu.ph>
2026-04-27 23:02:05 +0800

    feat: added taskbar and workspace component

--- mini-os-sandbox/src/App.jsx
+++ mini-os-sandbox/src/App.jsx
@@ -3,7 +3,8 @@ import './App.css';
 export default function App(){
   return (
     <div className='Desktop'>
-      <h1>WELCOME TO MY DESKTOP</h1>
+      <Workspace/>
+      <Taskbar />
     </div>
   );
 }
