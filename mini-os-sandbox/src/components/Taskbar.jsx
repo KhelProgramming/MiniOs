e125f10224cf01429b6542f5bd044252607a2007
Michael Rey Belga <mrab2023-5629-60654@bicol-u.edu.ph>
2026-04-27 23:02:29 +0800

    feat: added taskbar component

---
+++ mini-os-sandbox/src/components/Taskbar.jsx
@@ -0,0 +1,7 @@
+export default function Taskbar(){
+    return(
+        <div className='taskbar'>
+            { /* This is the component for the lower taskbar in the desktop */}
+        </div>
+    )
+}
