ba114c26546295f5d850586b873c4fe9f585edfd
Michael Rey Belga <mrab2023-5629-60654@bicol-u.edu.ph>
2026-04-27 23:03:29 +0800

    feat: added workspace component

---
+++ mini-os-sandbox/src/components/Workspace.jsx
@@ -0,0 +1,7 @@
+export default function workspace(){
+    return(
+        <div className='workspace'>
+            {/* All of the applications and the background will go here.*/}
+        </div>
+    )
+}
