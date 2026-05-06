6f381fa22212531eac3d674d7115fd49577a98b4
Michael Rey Belga <mrab2023-5629-60654@bicol-u.edu.ph>
2026-05-06 17:59:36 +0800

    feat: added dynamic system time

--- mini-os-sandbox/src/components/Taskbar.jsx
+++ mini-os-sandbox/src/components/Taskbar.jsx
@@ -3,10 +3,23 @@ import './Taskbar.css';
 
 export default function Taskbar(){
 
+    const [time, setTime] = useState(new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'}))
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTime(new Date().toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'}))
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, []);
+
     return(
         <div className='taskbar-container'>
             <div className='taskbar-left'>
-                <button className='taskbar-button'>
+                <button className='start-btn'>
+                    <svg viewbox="0 0 88 88" width='24' height='24'>
+                        <path fill='currentColor' d='M0 12.402l35.687-4.86.016 34.423-35.67.203zm35.67 33.528l.027 34.453-35.697-4.892v-29.764zm4.316-34.536l48.014-6.894v37.291h-48.014zm48.014 38.303l-.014 37.291-48 6.891-.014-34.331z' />
+                    </svg>
 
                 </button>
             </div>
@@ -16,7 +29,7 @@ export default function Taskbar(){
             </div>
 
             <div className='taskbar-tray'>
-                <span>12:00PM</span>
+                <span>{time}</span>
             </div>
         </div>
     )
