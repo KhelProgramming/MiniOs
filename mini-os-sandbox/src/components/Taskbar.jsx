c6285656fd047a5d3ed38a0115a4162a74a89331
Michael Rey Belga <mrab2023-5629-60654@bicol-u.edu.ph>
2026-05-06 17:53:07 +0800

    feat: initialized taskbar

--- mini-os-sandbox/src/components/Taskbar.jsx
+++ mini-os-sandbox/src/components/Taskbar.jsx
@@ -1,7 +1,23 @@
+import { useState, useEffect } from 'react';
+import './Taskbar.css';
+
 export default function Taskbar(){
+
     return(
-        <div className='taskbar'>
-            { /* This is the component for the lower taskbar in the desktop */}
+        <div className='taskbar-container'>
+            <div className='taskbar-left'>
+                <button className='taskbar-button'>
+
+                </button>
+            </div>
+
+            <div className='taskbar-center'>
+
+            </div>
+
+            <div className='taskbar-tray'>
+                <span>12:00PM</span>
+            </div>
         </div>
     )
 }
