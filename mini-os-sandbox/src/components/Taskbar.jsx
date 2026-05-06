7c016ae21e646633db5b592b56f9c5814164bf65
Michael Rey Belga <mrab2023-5629-60654@bicol-u.edu.ph>
2026-05-06 18:29:52 +0800

    chore: slight tweak

--- mini-os-sandbox/src/components/Taskbar.jsx
+++ mini-os-sandbox/src/components/Taskbar.jsx
@@ -17,7 +17,7 @@ export default function Taskbar(){
         <div className='taskbar-container'>
             <div className='taskbar-left'>
                 <button className='start-btn'>
-                    <svg viewbox="0 0 88 88" width='24' height='24'>
+                    <svg viewBox="0 0 88 88" width='24' height='24'>
                         <path fill='currentColor' d='M0 12.402l35.687-4.86.016 34.423-35.67.203zm35.67 33.528l.027 34.453-35.697-4.892v-29.764zm4.316-34.536l48.014-6.894v37.291h-48.014zm48.014 38.303l-.014 37.291-48 6.891-.014-34.331z' />
                     </svg>
 
